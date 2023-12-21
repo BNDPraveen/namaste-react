0115eec0aa09ff523e00f9a420ccad40d8fb14d1
BNDPraveen <praveenbabi124@gmail.com>
2023-12-22 02:53:47 +0530

    episode-01

---
+++ README.md
@@ -0,0 +1,3 @@
+# Namaste React
+
+s
---
+++ app.js
@@ -0,0 +1,16 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2tag"),
+  ]),
+]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.querySelector(".root"));
+
+root.render(parent);
